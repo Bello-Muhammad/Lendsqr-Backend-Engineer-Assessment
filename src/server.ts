48a3fdea3ee41e8a5e3bedd4dcd3f22bcc17aab9
Bello Muhammad Bello <bellob253@gmail.com>
2026-07-14 06:49:07 +0100

    chore: implement automatic migration outstanding database structure

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import * as dotenv from "dotenv";
 import walletRoutes from "./routes/wallet.routes.js"
+import { db } from "./database/database.js";
 
 dotenv.config();
 const app = express();
@@ -10,6 +11,14 @@ app.use(express.json());
 
 //routes entry
 app.use('/api', walletRoutes);
+
+//automatic database migration
+await db.migrate.latest().then(() => {
+    console.log("Database migration completed successfully.");
+}).catch((error: Error) => {
+    console.log("Database migration failed:", error);
+    process.exit(1);
+});
 
 app.listen(PORT, () => {
     console.log(`server listening on port ${PORT}`)
